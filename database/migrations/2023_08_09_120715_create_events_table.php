716f82734fb033b5e78c83dfde6b52bbe9d0fd5a
naffzzz <naffzzz@gmail.com>
2023-08-09 19:40:17 +0700

    init

---
+++ database/migrations/2023_08_09_120715_create_events_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('creator_id');
+            $table->unsignedBigInteger('price');
+            $table->date('event_date');
+            $table->boolean('is_open');
+            $table->string('status');
+            $table->date('open_order_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
